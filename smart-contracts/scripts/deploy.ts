732fc37ba84b503359277c4a55af770bebf27edb
Pierre GUÉVENEUX <pierregue75@gmail.com>
2022-11-18 14:29:29 -0800

    created graph

--- smart-contracts/scripts/deploy.ts
+++ smart-contracts/scripts/deploy.ts
@@ -1,17 +1,15 @@
 import { ethers } from "hardhat";
 
 async function main() {
-
-  const Lock = await ethers.getContractFactory("frontProposal");
+  
+  const Dao = await ethers.getContractFactory("umaInteract");
   //"0x2Ff10419Fd92498b1143a96Aa0374E9d247B7CBA" mumbai
-  const lock = await Lock.deploy();
-  const accounts = await ethers.getSigners()
-  await lock.deployed();
-  console.log(lock.address)
-  await lock.whitelist(accounts[1])
-  // await lock.whitelist(accounts[3].address)
-  // await lock.connect(accounts[3]).proposeFirstFront("https://djkscdfhjeks4r6H8j98g345.ipns.dweb.link/")  
-  await lock.connect(accounts[1]).proposeFirstFront("https://k51qzi5uqu5dgc7jdelv14mcma76yen9kaupekf5yhdncsrjuyn7hvxom4ig0u.ipns.dweb.link/")
+  const dao = await Dao.deploy()
+
+  await dao.submitDeployment("testr","0x7FC5f4f5bE07b698365DA975218909195404eF89")
+  console.log(await dao.propDetails("testr"))
+  await dao.requestData("testr");
+  console.log(dao.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
